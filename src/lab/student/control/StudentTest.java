4cb5653618db3224b7496cad4d52b2225e2c14ba
oyushik <craven_sawdust.9i@icloud.com>
2025-04-24 09:17:00 +0900

    student

---
+++ src/lab/student/control/StudentTest.java
@@ -0,0 +1,9 @@
+package lab.student.control;
+
+import lab.student.entity.Student;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student1 = new Student("202200000", "김민수", "컴퓨터공학", 3);
+    }
+}
---
+++ src/lab/student/entity/Student.java
@@ -0,0 +1,60 @@
+package lab.student.entity;
+
+public class Student {
+    private String studentId;
+    private String name;
+    private String major;
+    private int grade;
+
+    public Student(String studentId, String name, String major, int grade) {
+        this.studentId = studentId;
+        this.name = name;
+        this.major = major;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId='" + studentId + '\'' +
+                ", name='" + name + '\'' +
+                ", major='" + major + '\'' +
+                ", grade=" + grade + "학년" +
+                '}';
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        if (1 < grade || grade < 4) {
+            System.out.println("학년은 1~4 사이여야 합니다.");
+        }
+        this.grade = grade;
+    }
+}
